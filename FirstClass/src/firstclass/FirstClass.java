01bfc2ac2a3f023b9fcf266b6a319f0a4312b968
Felipe Serrano <felipeserrano@fia.ues.edu.sv>
2019-01-19 15:03:42 -0600

    Primera y segunda clase.

---
+++ FirstClass/src/firstclass/FirstClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstclass;
+
+/**
+ *
+ * @author FelipeSerrano
+ */
+public class FirstClass {
+     public static void main(String[] args) {
+         System.out.println("Hola Mundo");
+         System.out.println("Hola Mundo");
+         float nombre;
+         float Nombre;
+    }
+}
